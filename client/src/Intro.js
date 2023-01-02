e79c7d4ee30a10d95e94bfaa7ecd745360e59df6
Lu <lmpa.pt@gmail.com>
2023-01-03 00:34:53 +0100

    adding db

--- client/src/Intro.js
+++ client/src/Intro.js
@@ -2,6 +2,11 @@ export default function Intro() {
     return (
         <div className="m-5">
             <h3>Welcome!</h3>
+            <div className="catsDisplay d-flex justify-content-center mt-5">
+                <img className="m-2 catFrame" src="img/cat1.jpg" width="400"/>
+                <img className="m-2 catFrame" src="img/cat2.jpg" width="400"/>
+                <img className="m-2 catFrame" src="img/cat3.jpg" width="400"/>
+            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const mongo = require('mongodb').MongoClient;
 
-const PORT = 8000
+async function accessDb(operation, arguments){
 
-app.listen(PORT, function (){
-    console.log(`Server started running on http://localhost:${PORT}`)
-})
+    const URL = "mongodb://localhost:27017/votingdb"
+    
+    try {
+        mongo.connect(URL, function(err, db){
+            const data = db.db("votingdb");
+            console.log(data.collection("cat").find({}).toArray(function(err, res){
+                try{
+                    console.log(res)
+                }
+                catch(err){
+                    throw err;
+                }
+            }))
+        })
+     
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function main(){
+
+    
+    const PORT = 8000;
+    
+    const app = express();
+    
+    accessDb("", "")
+    app.listen(PORT, function (){
+        console.log(`Server started running on http://localhost:${PORT}`);
+    });
+}
+
+main()
